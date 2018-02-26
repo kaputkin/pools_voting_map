dc2b4cd7c34092400efbbcf03838109bafa05cc9
Ari <alk511@ad.nyu.edu>
2018-02-26 10:44:23 -0500

    2/26/18

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,23 @@ var map =
     L.tileLayer('https://cartodb-basemaps-{s}.global.ssl.fastly.net/dark_nolabels/{z}/{x}/{y}.png', {
     	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> &copy; <a href="http://cartodb.com/attributions">CartoDB</a>',
     	subdomains: 'abcd',
-    	maxZoom: 19,
+      minZoom: 8,
+    	maxZoom: 14,
     }).addTo(map);
 
 //add raster tiles hosted on github  --- not working ---
 
     // L.tileLayer('http://github.com/kaputkin/pools_voting_map/tree/master/data/data/{z}/{x}/{y}.png', {
-    //   maxZoom: 14
+    // minZoom: 8,
+    // maxZoom: 14
     // }).addTo(map);
 
-//add raster tiles hosted locally, but still tries to pull tiles not in dataset --- this does work ---
-L.tileLayer('http://localhost:8887/data/{z}/{x}/{y}.png', {
-  maxZoom: 14
-}).addTo(map);
+//add raster tiles hosted locally. Still tries to pull tiles not in dataset   --- this does work ---
+
+    // L.tileLayer('http://localhost:8887/data/{z}/{x}/{y}.png', {
+    // minZoom: 8,
+    // maxZoom: 14
+    // }).addTo(map);
 
 
 map.createPane('ED');
@@ -75,26 +79,26 @@ $.getJSON(votinged, function (geojson){
           pane: 'ED',
       };
   }
-//
-//   var poolArray = []  // empty array
-//   pool_data.forEach(function(poolObject) {
-//     var latlon = [poolObject.Y, poolObject.X];
-//     var options = {
-//       renderer: myRenderer,
-//       radius: 2,
-//       stroke: false,
-//       fillOpacity: .1,
-//       fillColor: '#1254ab',
-//       pane:'Pools',
-//     };
-//   poolArray.push(L.circleMarker(latlon, options))
-// });
-//
-// var pools = L.featureGroup(poolArray).addTo(map);
-//
-// var Sectorslayer = {
-//   "Swimming Pools": pools,
-//
-// };
-//
-// L.control.layers(null,Sectorslayer,{collapsed:false, position: 'topright'}).addTo(map);
+
+  var poolArray = []  // empty array
+  pool_data.forEach(function(poolObject) {
+    var latlon = [poolObject.Y, poolObject.X];
+    var options = {
+      renderer: myRenderer,
+      radius: 2,
+      stroke: false,
+      fillOpacity: .1,
+      fillColor: '#1254ab',
+      pane:'Pools',
+    };
+  poolArray.push(L.circleMarker(latlon, options))
+});
+
+var pools = L.featureGroup(poolArray).addTo(map);
+
+var Sectorslayer = {
+  "Swimming Pools": pools,
+
+};
+
+L.control.layers(null,Sectorslayer,{collapsed:false, position: 'topright'}).addTo(map);
